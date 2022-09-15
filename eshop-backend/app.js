30f49462939eb6543edecd16034b89a6a9fea25e
Paing Pyae Thu <paingpyaethu@kirasoft.co>
2022-09-15 14:35:03 +0630

    update

--- eshop-backend/app.js
+++ eshop-backend/app.js
@@ -1,1 +1,12 @@
-console.log("I'm app js");
+const express = require("express");
+const app = express();
+
+require("dotenv/config");
+const api = process.env.API_URL;
+app.get(api + "/products", (req, res) => {
+  res.send("Hello API!");
+});
+app.listen(3000, () => {
+  console.log(api);
+  console.log("server is running http://localhost:3000");
+});
