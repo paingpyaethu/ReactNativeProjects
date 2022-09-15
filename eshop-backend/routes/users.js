17e8664274928b7929cf85bcf6aa6dfa146824a1
Paing Pyae Thu <paingpyaethu@kirasoft.co>
2022-09-15 23:27:35 +0630

    create users & get users

--- eshop-backend/routes/users.js
+++ eshop-backend/routes/users.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const { User } = require("../models/user");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 router.get("/", async (req, res) => {
-  const userList = await User.find();
+  const userList = await User.find().select("-passwordHash");
   if (!userList) {
     res.status(500).json({ success: false, message: "User not found" });
   }
   res.send(userList);
+});
+
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id).select("-passwordHash");
+  if (!user)
+    return res
+      .status(400)
+      .json({ success: false, message: "User with the given id not found" });
+
+  res.status(200).send(user);
+});
+
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+
+  user = await user.save();
+
+  if (!user)
+    return res
+      .status(400)
+      .json({ success: false, message: "User cannot be created" });
+  res.send(user);
 });
 
 module.exports = router;
