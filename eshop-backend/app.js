e34ca4653577ba1518bac8c095b301ad6ec3e460
Paing Pyae Thu <paingpyaethu@kirasoft.co>
2022-09-15 14:49:39 +0630

    api call & parsing json data

--- eshop-backend/app.js
+++ eshop-backend/app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+//Middleware
+app.use(bodyParser.json());
 
 require("dotenv/config");
 const api = process.env.API_URL;
-app.get(api + "/products", (req, res) => {
-  res.send("Hello API!");
+
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "hair dresser",
+    image: "some url",
+  };
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
 });
 app.listen(3000, () => {
   console.log(api);
