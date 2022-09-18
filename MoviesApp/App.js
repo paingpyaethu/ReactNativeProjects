66f5462620580638276396c77bf49f146fc0af63
Paing Pyae Thu <paingpyaethu@kirasoft.co>
2022-09-19 00:34:29 +0630

    setup navigation

--- MoviesApp/App.js
+++ MoviesApp/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import React, {useEffect, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
 const App = () => {
   return (
-    <SafeAreaView style={styles.appContainer}>
-      <HomeScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
