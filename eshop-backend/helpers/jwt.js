9d6f459044e93ea24160e053ba43943f8db66750
Paing Pyae Thu <paingpyaethu@kirasoft.co>
2022-09-28 21:22:38 +0630

    update users

--- eshop-backend/helpers/jwt.js
+++ eshop-backend/helpers/jwt.js
@@ -1,11 +1,12 @@
 const { expressjwt: jwt } = require("express-jwt");
 
 function authJwt() {
-  const secret = process.env.USER_SECRET;
+  const secret = process.env.JWT_SECRET;
   const api = process.env.API_URL;
   return jwt({
     secret,
     algorithms: ["HS256"],
+    isRevoked: isRevoked,
   }).unless({
     path: [
       { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
@@ -14,6 +15,13 @@ function authJwt() {
       `${api}/users/register`,
     ],
   });
+}
+
+async function isRevoked(req, token) {
+  if (!token.payload.isAdmin) {
+    return true;
+  }
+  return undefined;
 }
 
 module.exports = authJwt;
--- eshop-backend/routes/users.js
+++ eshop-backend/routes/users.js
@@ -87,6 +87,7 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign(
       {
         userId: user.id,
+        isAdmin: user.isAdmin,
       },
       secret,
       { expiresIn: "1d" }
@@ -102,6 +103,37 @@ router.post("/login", async (req, res) => {
       .status(400)
       .json({ success: false, message: "Email or password is incorrect" });
   }
+});
+
+router.delete("/:id", async (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+    .then((user) => {
+      if (user) {
+        return res
+          .status(200)
+          .json({ success: true, message: "User removed successfully" });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "User not found" });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
+
+router.get(`/get/count`, async (req, res) => {
+  const userCount = await User.countDocuments((count) => count);
+
+  if (!userCount) {
+    return res.status(500).json({
+      success: false,
+    });
+  }
+  res.send({
+    userCount: userCount,
+  });
 });
 
 module.exports = router;
