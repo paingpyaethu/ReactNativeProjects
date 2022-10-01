fb1df8d2da5280cecab47e40b801a1d7f8f99cc0
Paing Pyae Thu <paingpyaethu@kirasoft.co>
2022-10-01 22:41:06 +0630

    excluding uploads folder from authentication

--- eshop-backend/app.js
+++ eshop-backend/app.js
@@ -14,6 +14,7 @@ app.options("*", cors());
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 app.use(authJwt());
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 app.use(errorHandler);
 
 //Routes
