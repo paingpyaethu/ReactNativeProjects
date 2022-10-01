9bc0248c6a3543f7f4eb8828c1514be278d41c4f
Paing Pyae Thu <paingpyaethu@kirasoft.co>
2022-10-01 21:55:17 +0630

    validating uploaded file types

--- eshop-backend/routes/products.js
+++ eshop-backend/routes/products.js
@@ -5,12 +5,25 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const multer = require("multer");
 
+const FILE_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/uploads");
+    const isVaild = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error("Invalid Image Type");
+
+    if (isVaild) {
+      uploadError = null;
+    }
+    cb(uploadError, "public/uploads");
   },
   filename: function (req, file, cb) {
     const fileName = file.originalname.split(" ").join("-");
+    const extension = FILE_TYPE_MAP[file.mimetype];
     cb(null, `${fileName}-${Date.now()}.${extension}`);
   },
 });
@@ -45,6 +58,10 @@ router.get(`/:id`, async (req, res) => {
 router.post(`/`, uploadOptions.single("image"), async (req, res) => {
   const category = await Category.findById(req.body.category);
   if (!category) return res.status(400).send("Invalid Category");
+
+  const file = req.file;
+  if (!file) return res.status(400).send("No Image In Request!");
+
   const file_name = req.file.filename;
   const basePath = `${req.protocol}://${req.get("host")}/public/uploads/`;
 
