12944db3d5c91881f42da54008d702dc8bf97ec7
Paing Pyae Thu <paingpyaethu@kirasoft.co>
2022-10-29 18:55:19 +0630

    update server port

--- eshop-backend/app.js
+++ eshop-backend/app.js
@@ -42,7 +42,15 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-app.listen(3000, () => {
-  console.log(api);
-  console.log("server is running http://localhost:3000");
+
+//Development
+// app.listen(3000, () => {
+//   console.log(api);
+//   console.log(`server is running http://${process.env.IP}:3000`);
+// });
+
+// Production
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
 });
