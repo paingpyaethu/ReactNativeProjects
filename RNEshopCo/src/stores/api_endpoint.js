ed2881e32ba8053e35a47c8f9841f2675115d3ea
Paing Pyae Thu <paingpyaethu@kirasoft.co>
2022-12-05 19:31:07 +0630

    update

--- RNEshopCo/src/stores/api_endpoint.js
+++ RNEshopCo/src/stores/api_endpoint.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://rn-eshop-production.up.railway.app/api/v1';
-export const BASE_URL = 'http://192.168.1.4:3000/api/v1';
+export const BASE_URL = 'https://rn-eshop-production.up.railway.app/api/v1';
+// export const BASE_URL = 'http://192.168.1.4:3000/api/v1';
