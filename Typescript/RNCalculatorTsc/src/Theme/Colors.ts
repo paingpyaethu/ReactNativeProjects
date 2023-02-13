200c8c679b4d5a18665c35f11e05691a0d8bfca3
paingpyaethu <paingpyaethu@gmail.com>
2023-02-13 13:44:56 +0630

    update rn calc

---
+++ Typescript/RNCalculatorTsc/src/Theme/Colors.ts
@@ -0,0 +1,11 @@
+export const myColors = {
+  light: '#f1f2f3',
+  dark: '#17171c',
+  blue: '#4e5efc',
+  btnGray: '#4e505f',
+  btnDark: '#2e2f38',
+  gray: '#747477',
+  black: '#000',
+  white: '#fff',
+  result: '#46d5b2',
+};
